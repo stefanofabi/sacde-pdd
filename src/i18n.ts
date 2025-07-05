7f5090a9d4ee721ccb48b342ba4fc951bdeafbcc
Stefano Fabi <stefanofabi96@gmail.com>
2025-07-05 14:15:18 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,14 +1,18 @@
 import {notFound} from 'next/navigation';
 import {getRequestConfig} from 'next-intl/server';
+import {getLocale} from 'next-intl/server';
  
 // Can be imported from a shared config
 const locales = ['en', 'es'];
  
-export default getRequestConfig(async ({locale}) => {
+export default getRequestConfig(async () => {
+  const locale = await getLocale();
+ 
   // Validate that the incoming `locale` parameter is valid
   if (!locales.includes(locale as any)) notFound();
  
   return {
+    locale,
     messages: (await import(`../messages/${locale}.json`)).default
   };
 });
