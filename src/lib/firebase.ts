bc64882f979b6ca1717df40135e4f8f3f48531a9
stefanofabi96 <stefanofabi96@gmail.com>
2025-07-21 01:23:28 +0000

    Agregar comentario a configuracion de firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,6 +3,7 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "YOUR_API_KEY",
   authDomain: "your-project-id.firebaseapp.com",
